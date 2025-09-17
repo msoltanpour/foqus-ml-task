a0f1229e87305f01d63c386d9f0bf9f87d96c8f4
Mohsen Soltanpour <msoltanp@ualberta.ca>
2025-09-17 14:36:11 -0400

    style: fix imports with ruff --fix

--- preview_task1_dataset.py
+++ preview_task1_dataset.py
@@ -40,9 +40,13 @@ import torch
 
 matplotlib.use("Agg")  # ensure headless save
 import matplotlib.pyplot as plt
-
 from foqus_ml_task.datasets import RandomPhantomDataset
-from foqus_ml_task.transforms import Augmentation, EquispacedUndersample, Normalize, ToCompatibleTensor
+from foqus_ml_task.transforms import (
+    Augmentation,
+    EquispacedUndersample,
+    Normalize,
+    ToCompatibleTensor,
+)
 
 
 def parse_args() -> argparse.Namespace:
